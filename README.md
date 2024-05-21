e774f98c1726ba980bcf7ff07ec89092bb1e3f1b
unknown <sachittanisha@gmail.com>
2024-05-21 23:16:10 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# EXPLORE EASE
+
+## Description
+Explore Ease is a web application designed to help users create personalized travel itineraries based on their preferences, budget, and travel goals. The application uses the OpenAI Groq API to generate itineraries based on user input.
+
+## Table of Contents
+1. Features
+2. Installation
+3. Usage
+4. Database Structure
+5. API Integration
+6. Code Structure
+
+## Features
+- User Authentication
+- Personalized travel itinerary generation based on user input
+- Save, view and delete saved itineraries
+- Provide and view feedback on generated itineraries
+- Edit user profile
+- Delete user account and all associated data
+
+## Installation
+1. Clone this repository: https://github.com/TanishaKothari/explore-ease.git
+2. Navigate to the project directory: `cd explore-ease`
+3. Install the required dependencies: `pip install flask werkzeug sqlite3 flask_session openai`
+4. Run the application: `python app.py`
+
+## Usage
+1. Acess the web application by navigating to http://localhost:5000 in your browser.
+2. Register an account by clicking the "Register" link and providing a valid username and password.
+3. Log in to your account using the details provided during the registration.
+4. Fill out the travel preferences form to generate a personalized travel itinerary.
+5. Generate a new itinerary with the same inputs using the "Regenerate" button, save the generated itinerary using the "Save Plan" button, or provide feedback on the generated itinerary by clicking the "Give feedback" button and filling out the feedback form.
+6. View all saved itineraries in the "Saved Itineraris" section. Unsave itineraries by clicking the "Delete" button.
+7. Edit your profile or delete account by clicking on your username and selecting "Edit Profile".
+8. Logout by clicking on your username and selecting "Logout".
+
+## Database Structure
+The application uses a SQLite database named `explore_ease.db` to store user information and user input. The database contains the following tables:
+- users: Stores user information such as the user's ID, username and hashed password.
+- user_input: Stores user input entered in the itinerary generation form until the user logs out.
+- saved_plans: Stores saved itineraries with user ID and plan ID.
+- feedback: Stores user feedback along with feedback's ID and user ID.
+
+## API Integration
+The application integrates with the OpenAI Groq API to generate personalized travel itineraries based on user input. The API is used to create a custom AI agent and task, which in turn generates the itinerary based on the provided criteria.
+
+## Code Structure
+The application is built using the Flask framework in Python. The code is organized into multiple files, including:
+- app.py: The main Flask application file, which contains the routes, views, and functions for the web application.
+- templates/: A directory containing all the HTML templates used by the application.
+- static/: A directory containing the CSS file.
---
+++ app.py
@@ -0,0 +1,421 @@
+from flask import Flask, flash, render_template, redirect, session, request
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+from utilities import login_required
+from flask_session import Session
+import os
+from crewai import Agent, Task, Crew, Process
+
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+con = sqlite3.connect('explore_ease.db')
+cursor = con.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, 
+    hash TEXT NOT NULL)''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user_input (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL,
+        destination TEXT NOT NULL, budget INTEGER, duration INTEGER, pace TEXT,
+        months TEXT, interests TEXT, dietary_restrictions TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS saved_plans (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        user_id INTEGER NOT NULL, plan_details TEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS feedback (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        user_id INTEGER NOT NULL, feedback TEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id))''')
+
+cursor.close()
+con.close()
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+# Validate login
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Must provide a username")
+            return redirect("/login")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("Must provide a password")
+            return redirect("/login")
+
+        # Query database for username
+        cursor.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
+        rows = cursor.fetchall()
+
+        cursor.close()
+        con.close()
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0][2], request.form.get("password")):
+            print(rows)
+            flash("Invalid username and/or password")
+            return redirect("/login")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0][0]
+        session["username"] = rows[0][1]
+
+        # Redirect user to home page
+        return redirect("/")
+    
+    return render_template("login.html")
+
+
+# Log user out and delete stored user input
+@app.route("/logout")
+def logout():
+    """Log user out"""
+    # Delete stored user input
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM user_input WHERE user_id = ?", (session["user_id"],))
+    con.commit()
+    cursor.close()
+    con.close()
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/login")
+
+
+# Create new user account and store in users table
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+
+    if request.method == "POST":
+        # Get user inputs from the form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Validate inputs
+        if not username:
+            flash("Username cannot be blank.")
+            return redirect("/register")
+        if cursor.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchone() is not None:
+            flash("Username already exists.")
+            return redirect("/register")
+        if not password or not confirmation:
+            flash("Password fields cannot be blank.")
+            return redirect("/register")
+        if password != confirmation:
+            flash("Passwords do not match.")
+            return redirect("/register")
+
+        hashed_password = generate_password_hash(password)
+        cursor.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (username, hashed_password))
+        con.commit()
+        return redirect("/login")
+
+    cursor.close()
+    con.close()
+    return render_template("register.html")
+
+
+# Update user input and generate itinerary as per new input
+@app.route("/suggestions", methods=['GET', 'POST'])
+@login_required
+def get_suggestions():
+    if request.method == 'POST':
+        con = sqlite3.connect('explore_ease.db')
+        cursor = con.cursor()
+        # Get user input from the form
+        input_dict = {
+            'duration': request.form['duration'] + ' days',
+            'destination': request.form['destination'],
+            'months': request.form['months'],
+            'budget': request.form['budget'],
+            'pace': request.form['pace'],
+            'interests': request.form.getlist('interests'),
+            'dietary_restrictions': request.form.getlist('dietary_restrictions'),
+        }
+        cursor.execute("SELECT * FROM user_input WHERE user_id = ?", (session["user_id"],))
+        user_input = cursor.fetchone()
+
+        if user_input is not None:
+            cursor.execute('''
+                UPDATE user_input SET duration =?, destination =?, months =?, budget =?, pace =?, interests =?, dietary_restrictions =? WHERE user_id =?''',  
+                (input_dict['duration'], input_dict['destination'], input_dict['months'], input_dict['budget'], input_dict['pace'],
+                ','.join(input_dict['interests']), ','.join(input_dict['dietary_restrictions']), session["user_id"]))
+            con.commit()
+        else:
+            cursor.execute('''
+                INSERT INTO user_input (user_id, duration, destination, months, budget, pace, interests, dietary_restrictions)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', 
+                (session["user_id"], input_dict['duration'], input_dict['destination'], input_dict['months'], input_dict['budget'], input_dict['pace'],
+                ','.join(input_dict['interests']), ','.join(input_dict['dietary_restrictions'])))
+            con.commit()
+        
+        itinerary = generate_itinerary()
+
+        cursor.close()
+        con.close()
+
+        return render_template('suggestions.html', generated_plan=itinerary)
+    return redirect("/")
+
+
+# Generate new itinerary using same input
+@app.route("/regenerate", methods=['POST'])
+@login_required
+def regenerate():
+    itinerary = generate_itinerary()
+    return render_template('suggestions.html', generated_plan=itinerary)
+
+
+# Save itinerary for user in saved_plans table
+@app.route("/save_plan", methods=['POST'])
+@login_required
+def save_plan():
+    # Store the plan in the database
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+    cursor.execute('''
+        INSERT INTO saved_plans (user_id, plan_details)
+        VALUES (?, ?)
+    ''', (session["user_id"], generated_itinerary))
+    con.commit()
+    cursor.close()
+    con.close()
+
+    flash("Plan saved successfully!")
+    return redirect("/")
+
+
+# Show all saved itineraries for the user
+@app.route("/saved_plans")
+@login_required
+def saved_plans():
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+    cursor.execute("SELECT id, plan_details FROM saved_plans WHERE user_id = ?", (session["user_id"],))
+    saved_plans = cursor.fetchall()
+    cursor.close()
+    con.close()
+
+    return render_template('saves.html', saved_plans=saved_plans)
+
+
+# Delete plan from saved_plans table
+@app.route("/delete_saved_plans", methods=['POST'])
+@login_required
+def delete_saved_plans():
+    plan_id = request.form.get('plan_id')
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+    cursor.execute("DELETE FROM saved_plans WHERE id = ? AND user_id = ?", (plan_id, session["user_id"]))
+    con.commit()
+    cursor.close()
+    con.close()
+    flash("Plan deleted successfully!")
+    return redirect("/")
+
+
+# Allow user to give feedback on the itinerary and display all stored feedbacks below form
+@app.route("/feedback", methods=['GET', 'POST'])
+@login_required
+def feedback():
+    if request.method == 'POST':
+        con = sqlite3.connect('explore_ease.db')
+        cursor = con.cursor()
+        # Get user input from the form
+        feedback = request.form.get("feedback")
+        cursor.execute('''
+            INSERT INTO feedback (user_id, feedback)
+            VALUES (?, ?)
+        ''', (session["user_id"], feedback))
+        con.commit()
+        flash("Feedback submitted successfully!")
+        return redirect("/")
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+    cursor.execute("SELECT feedback FROM feedback")
+    feedbacks = cursor.fetchall()
+    cursor.close()
+    con.close()
+    return render_template('feedback.html', feedbacks=feedbacks)
+
+
+@app.route("/edit_profile", methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    return render_template('edit_profile.html')
+
+
+# Allow user to change password if all inputs are correct
+@app.route("/change_password", methods=['GET', 'POST'])
+@login_required
+def change_password():
+    if request.method == 'POST':
+        con = sqlite3.connect('explore_ease.db')
+        cursor = con.cursor()
+        
+        cursor.execute("SELECT * FROM users WHERE username = ?", (session["username"],))
+        rows = cursor.fetchall()
+
+        # Ensure current password entered is correct
+        if not check_password_hash(rows[0][2], request.form.get("current_password")):
+            flash("Invalid password")
+            return redirect("/edit_profile")
+        
+        new_password = request.form.get("new_password")
+        confirm_new_password = request.form.get("confirm_password")
+
+        # Ensure new password and confirm new password match
+        if new_password != confirm_new_password:
+            flash("Passwords do not match")
+            return redirect("/edit_profile")
+        cursor.execute('''
+            UPDATE users SET hash = ? WHERE id = ?
+            ''', (generate_password_hash(new_password), session["user_id"]))
+        con.commit()
+        cursor.close()
+        con.close()
+        flash("Password changed successfully!")
+        return redirect("/login")
+    return render_template('edit_profile.html')
+
+
+# Allow user to change username if new one is available
+@app.route("/change_username", methods=['GET', 'POST'])
+@login_required
+def change_username():
+    if request.method == 'POST':
+        con = sqlite3.connect('explore_ease.db')
+        cursor = con.cursor()
+
+        # Change username if new one is available
+        new_username = request.form.get("new_username")
+        if cursor.execute("SELECT * FROM users WHERE username = ?", (new_username,)).fetchone() is not None:
+            flash("Username already exists.")
+            return redirect("/edit_profile")
+        else:
+            cursor.execute('''
+                UPDATE users SET username =? WHERE id = ?''', (new_username, session["user_id"]))
+            con.commit()
+            cursor.close()
+            con.close()
+            flash("Username changed successfully!")
+            return redirect("/login")
+    return render_template('edit_profile.html')
+
+
+# Allow user to delete account and delete all data associated with it from users, user_input and saved_plans tables
+@app.route("/delete_account", methods=['GET', 'POST'])
+@login_required
+def delete_account():
+    if request.method == 'POST':
+        con = sqlite3.connect('explore_ease.db')
+        cursor = con.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE username = ?", (session["username"],))
+        rows = cursor.fetchall()
+
+        # Ensure current password entered is correct
+        if not check_password_hash(rows[0][2], request.form.get("current_password")):
+            flash("Invalid password")
+            return redirect("/edit_profile")
+        
+        cursor.execute('''
+            DELETE FROM users WHERE id = ?''', (session["user_id"],))
+        cursor.execute('''
+            DELETE FROM user_input WHERE user_id = ?''', (session["user_id"],))
+        cursor.execute('''
+            DELETE FROM saved_plans WHERE user_id = ?''', (session["user_id"],))
+        con.commit()
+        cursor.close()
+        con.close()
+        flash("Account deleted successfully!")
+        return redirect("/register")
+    return render_template('edit_profile.html')
+
+# Generate itinerary based on the user input found in the table user_input using openAI through groq API.
+def generate_itinerary():
+    con = sqlite3.connect('explore_ease.db')
+    cursor = con.cursor()
+
+    # Retrieve user input from the database
+    cursor.execute("SELECT * FROM user_input WHERE user_id = ?", (session["user_id"],))
+    user_input = cursor.fetchone()
+
+    os.environ["OPENAI_API_BASE"] = "https://api.groq.com/openai/v1"
+    os.environ["OPENAI_MODEL_NAME"] = "llama3-70b-8192"
+    os.environ["OPENAI_API_KEY"] = "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"
+
+    criteria = f"duration: {user_input[4]}, destination: {user_input[2]}, first month: {user_input[6]}, budget: {user_input[3]}, pace: {user_input[5]}, interests: {user_input[7]}, dietary restrictions: {user_input[8]}"
+
+    generator = Agent(
+        role = "travel itinerary generator",
+        goal = "Create a personalized travel itinerary.",
+        backstory = "You are an AI assistant. You have to create a personalized travel itinerary.",
+        verbose = True,
+        allow_delegation = False
+    )
+
+    generate_itinerary = Task(
+        description = f"Generate a travel itinerary based on the following criteria: {criteria}. Also consider the places' weather for the duration of the trip. If no specific criteria is provided, generate based on the weather situations of different places.",
+        agent = generator,
+        expected_output = "a personalized travel itinerary"
+    )
+
+    crew = Crew(
+        agents = [generator],
+        tasks = [generate_itinerary],
+        verbose = 2,
+        process = Process.sequential
+    )
+
+    itinerary = crew.kickoff()
+
+    cursor.close()
+    con.close()
+
+    global generated_itinerary
+    generated_itinerary = itinerary.replace("*","")
+    return generated_itinerary
+
+if __name__ == '__main__':
+    app.run(debug=True)
