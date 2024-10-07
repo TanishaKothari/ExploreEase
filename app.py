c674b871477e4fbdd5691bf74c859f57b19a47a1
Tanisha Kothari <64086266+TanishaKothari@users.noreply.github.com>
2024-10-07 07:43:09 +0000

    Update

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask_session import Session
 import os
 from crewai import Agent, Task, Crew, Process
 
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -365,7 +368,7 @@ def generate_itinerary():
 
     os.environ["OPENAI_API_BASE"] = "https://api.groq.com/openai/v1"
     os.environ["OPENAI_MODEL_NAME"] = "llama3-70b-8192"
-    os.environ["OPENAI_API_KEY"] = "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"
+    os.environ["OPENAI_API_KEY"] = os.environ.get('key')
 
     criteria = f"duration: {user_input[4]}, destination: {user_input[2]}, first month: {user_input[6]}, budget: {user_input[3]}, pace: {user_input[5]}, interests: {user_input[7]}, dietary restrictions: {user_input[8]}"
 
